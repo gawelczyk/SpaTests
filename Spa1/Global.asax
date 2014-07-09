1499671eb4be9add83fe556878225af4812ddbec
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-09 13:33:24 +0200

    initial version, as created by VS2013

---
+++ Spa1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App.Spa1.Application" Language="C#" %>
---
+++ spa2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace spa2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
